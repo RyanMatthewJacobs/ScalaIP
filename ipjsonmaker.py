4a66e48f7efec8c95bb05a9e6823118cfdc3f06d
Julian Shalaby <jshalaby@ucsc.edu>
2021-02-27 16:44:02 -0800

    Update ipjsonmaker.py

--- ipjsonmaker.py
+++ ipjsonmaker.py
@@ -1,10 +1,8 @@
 import random
 
-ipList = []
 f = open("ipFile.json", "w")
 
 for i in range(10000):
     ip = ".".join(map(str, (random.randint(0, 255)
                             for _ in range(4))))
-    ipList.append(f"IPAddress: {ip}")
     f.write(f"{{\"IPAddress\": \"{ip}\"}}\n")
